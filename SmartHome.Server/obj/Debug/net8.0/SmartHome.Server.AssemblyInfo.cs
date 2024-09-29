3eb2d82f70c66e959244fe43bd0c6c768dd68dd6
Gabor Balla <gabor.balla@evosoft.com>
2024-09-29 19:22:07 +0200

    Fix gitignore

--- SmartHome.Server/obj/Debug/net8.0/SmartHome.Server.AssemblyInfo.cs
+++ SmartHome.Server/obj/Debug/net8.0/SmartHome.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartHome.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9aedb0d0871f497dd9fe5c1c031c7651ab71d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartHome.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartHome.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
